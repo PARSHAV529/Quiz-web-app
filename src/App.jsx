b51b7260247a0bc050883c7c9e51343e7836d5e0
Parshav <parshav143@gmail.com>
2024-03-01 15:33:22 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/header";
-import Quiz from './components/Quiz';
+import Header from "./components/Header.jsx";
+import Quiz from './components/Quiz.jsx';
 
 
 function App() {
